812ef8628706def102b5884d01afb977b90dae66
Tony Baker <this.tonybaker@gmail.com>
2019-04-02 17:12:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,18 +4,17 @@ class String
 
   def sentence?
     self.end_with?(".")
-
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\z/).count
   end
 end
